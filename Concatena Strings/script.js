6dcf04c3949b999988e056cf7b63758c6630018c
Allan Kildare <allankildare27@gmail.com>
2020-10-01 21:43:57 -0300

    pasta concatena strings criada

---
+++ Concatena Strings/script.js
@@ -0,0 +1,6 @@
+function concatenacao (string1, string2) {
+    var concatenaString = string1 + " " + string2
+    document.write(concatenaString)
+}
+
+concatenacao("Olá", "Mundo!") // imprime "Olá Mundo!" no index.html
