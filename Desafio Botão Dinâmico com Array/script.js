d43d49b92b00ee2e5cb8a25724ac54d35878b8ea
Allan Kildare <allankildare27@gmail.com>
2020-10-05 22:07:15 -0300

    legibilidade da funcao melhorada e gitignore

--- Desafio Botão Dinâmico com Array/script.js
+++ Desafio Botão Dinâmico com Array/script.js
@@ -1,10 +1,10 @@
-// Criar uma função que cria um botão dinâmico
+// Criar uma função que cria um botao dinâmico
 var nomeBotoes = ["Allan", "Marcus", "Henryque", "Lorenzo", "Amanda", "Gabriel"]
 
 function criaBotao () {
-    // Para cada nome no array, criar um botão que chame um alert com o próprio nome do botao
+    // Para cada nome no array, criar um botao que chame um alert com o próprio nome do botao
     for (var contador = 0; contador < nomeBotoes.length; contador++) {
-        document.write('<button onclick="alert(' + "'" + nomeBotoes[contador] + "'" + ')">' + nomeBotoes[contador] + "</button>")
+        document.write(`<button onclick="alert('` + nomeBotoes[contador] + `')">` + nomeBotoes[contador] + `</button>`)
     }
 }
 
