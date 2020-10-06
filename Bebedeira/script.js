af23e19abde8d50d6bfbe87b59d3cad239cb78e8
Allan Kildare <allankildare27@gmail.com>
2020-10-05 23:06:22 -0300

    pasta bebedeira excluida

--- Bebedeira/script.js
+++
@@ -1,9 +0,0 @@
-function idadeBeber (idadeUsuario, idadePermissao) {
-    if (idadeUsuario >= idadePermissao) {
-        document.write("Você pode beber")
-    } else {
-        document.write("Você não pode beber, lamento")
-    }
-}
-
-idadeBeber(14, 18) // imprime "Você não pode beber, lamento" ufaaa
