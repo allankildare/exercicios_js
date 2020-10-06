b1d2db0f578c6d5ab8a0c458d4774ee10dec7707
Allan Kildare <allankildare27@gmail.com>
2020-10-05 23:04:18 -0300

    funcao modificada, usa while

--- Desafio Botão Dinâmico com Array/script.js
+++ Desafio Botão Dinâmico com Array/script.js
@@ -1,9 +1,11 @@
-// Criar uma função que cria um botao dinâmico
+// Criar uma funçao que cria um botao dinamico
+// Solução com While
 var nomeBotoes = ["Allan", "Marcus", "Henryque", "Lorenzo", "Amanda", "Gabriel"]
 
 function criaBotao () {
-    // Para cada nome no array, criar um botao que chame um alert com o próprio nome do botao
-    for (var contador = 0; contador < nomeBotoes.length; contador++) {
+    var contador = -1
+    while (contador < (nomeBotoes.length - 1)) {
+        contador++
         document.write(`<button onclick="alert('` + nomeBotoes[contador] + `')">` + nomeBotoes[contador] + `</button>`)
     }
 }
