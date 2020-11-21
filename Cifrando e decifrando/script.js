fefcf6f2e4e95e883262028ffef71e431b79db22
Allan Kildare <allankildare27@gmail.com>
2020-11-21 03:18:13 -0300

    cifra de cesar concluida

--- Cifrando e decifrando/script.js
+++ Cifrando e decifrando/script.js
@@ -1,3 +1,8 @@
+/* OBS
+as duas proximas funcoes estao limitadas ao deslocamento 78 (com precisao), depois desse
+deslocamento, nao havera mais precisao.
+*/
+
 function cifraComCifraDeCesar( deslocamento, mensagem ) {
     let mensagemEncriptada = []
     let arrayTemp = []
@@ -19,13 +24,34 @@ function cifraComCifraDeCesar( deslocamento, mensagem ) {
     arrayTemp.forEach((e) => mensagemEncriptada.push(String.fromCharCode(e)))
     
     //return mensagemEncriptada.join('')
-    console.log(mensagemEncriptada.join(''))
+    return mensagemEncriptada.join('')
 }
 
 // deslocamento 2 e mensagem passados como parametro
 cifraComCifraDeCesar(2, "ABCDZ") // saida esperada: CDEFB
 
-/* Adendo
-essa funcao esta limitada ao deslocamento 78 com precisao, depois desse
-deslocamento, nao havera mais a perfeita precisao.
-*/
+function desencriptografar(deslocamento, mensagem) {
+    let descriptografada = []
+    let arrayTemp = []
+
+    for (let i = 0; i < mensagem.length; i++) {
+        let letraEncriptada = mensagem[i].charCodeAt() - deslocamento
+        
+        if (letraEncriptada < 65 && letraEncriptada >= 39) {
+            letraEncriptada += 26
+        } else if (letraEncriptada < 39 && letraEncriptada >= 13) {
+            letraEncriptada += 52
+        } else if (letraEncriptada < 13) {
+            letraEncriptada += 78
+        }
+
+        arrayTemp.push(letraEncriptada)
+    }
+
+    arrayTemp.forEach((e) => descriptografada.push(String.fromCharCode(e)))
+    
+
+    return descriptografada.join('')
+}
+
+desencriptografar(2, "CDEFB") // saida esperada: ABCDZ
